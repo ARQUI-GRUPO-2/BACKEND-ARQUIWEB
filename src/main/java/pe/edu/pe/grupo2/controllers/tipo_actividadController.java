3b898782ea82c57da4ba6873ac0179fbffe348fc
stephano-2002 <95435779+stephano-2002@users.noreply.github.com>
2024-09-01 15:30:30 -0500

    g

--- src/main/java/pe/edu/pe/grupo2/controllers/tipo_actividadController.java
+++ src/main/java/pe/edu/pe/grupo2/controllers/tipo_actividadController.java
@@ -3,31 +3,31 @@ package pe.edu.pe.grupo2.controllers;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import pe.edu.pe.grupo2.dtos.CentroReciclajeDTO;
-import pe.edu.pe.grupo2.serviceinterfaces.ICentroReciclajeService;
+import pe.edu.pe.grupo2.dtos.tipo_actividadDTO;
+import pe.edu.pe.grupo2.serviceinterfaces.tipo_actividadService;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/centros-de-reciclaje")
-public class CentroReciclajeController {
+public class tipo_actividadController {
 
     @Autowired
-    private ICentroReciclajeService tA;
+    private tipo_actividadService tA;
     @GetMapping
-    public List<CentroReciclajeDTO> listar() {
+    public List<tipo_actividadDTO> listar() {
 
         return tA.list().stream().map(x->{
             ModelMapper m=new ModelMapper();
-            return m.map(x, CentroReciclajeDTO.class);
+            return m.map(x, tipo_actividadDTO.class);
         }).collect(Collectors.toList());
-
-    @PostMapping
-    public void insertar(@RequestBody VehicleDTO dto) {
+        @PostMapping
+        public void insertar(@RequestBody VehicleDTO dto) {
             ModelMapper m = new ModelMapper();
             Vehicle ve = m.map(dto, Vehicle.class);
             tA.insert(ve);
 
+        }
     }
 }
