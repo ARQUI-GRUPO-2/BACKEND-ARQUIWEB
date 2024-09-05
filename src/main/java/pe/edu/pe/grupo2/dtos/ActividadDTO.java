6fe49730232cea132a954507f387e3cf31494145
M1guelVF <mvfernandez1492@gmail.com>
2024-09-04 19:39:01 -0500

    feat: Implement type of activity features and update activity

--- src/main/java/pe/edu/pe/grupo2/dtos/ActividadDTO.java
+++ src/main/java/pe/edu/pe/grupo2/dtos/ActividadDTO.java
@@ -1,6 +1,10 @@
 package pe.edu.pe.grupo2.dtos;
 
 
+import pe.edu.pe.grupo2.entities.CentroReciclaje;
+import pe.edu.pe.grupo2.entities.TipoActividad;
+import pe.edu.pe.grupo2.entities.User;
+
 public class ActividadDTO {
 
     private int idActividad;
@@ -11,64 +15,45 @@ public class ActividadDTO {
     private String cantidad;
     private String ubicacion;
 
-    public int getIdActividad() {
-        return idActividad;
-    }
+    private TipoActividad ta;
+    private CentroReciclaje cr;
+    private User u;
 
-    public void setIdActividad(int idActividad) {
-        this.idActividad = idActividad;
-    }
+    public int getIdActividad() {return idActividad;}
 
-    public String getFecha_recepcion() {
-        return fecha_recepcion;
-    }
+    public void setIdActividad(int idActividad) {this.idActividad = idActividad;}
 
-    public void setFecha_recepcion(String fecha_recepcion) {
-        this.fecha_recepcion = fecha_recepcion;
-    }
+    public String getFecha_recepcion() {return fecha_recepcion;}
 
-    public String getDescripcion() {
-        return descripcion;
-    }
+    public void setFecha_recepcion(String fecha_recepcion) {this.fecha_recepcion = fecha_recepcion;}
 
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
+    public String getDescripcion() {return descripcion;}
 
-    public String getNombre() {
-        return nombre;
-    }
+    public void setDescripcion(String descripcion) {this.descripcion = descripcion;}
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+    public String getNombre() {return nombre;}
 
-    public String getPuntos() {
-        return puntos;
-    }
+    public void setNombre(String nombre) {this.nombre = nombre;}
 
-    public void setPuntos(String puntos) {
-        this.puntos = puntos;
-    }
+    public String getPuntos() {return puntos;}
 
-    public String getCantidad() {
-        return cantidad;
-    }
+    public void setPuntos(String puntos) {this.puntos = puntos;}
 
-    public void setCantidad(String cantidad) {
-        this.cantidad = cantidad;
-    }
+    public String getCantidad() {return cantidad;}
 
-    public String getUbicacion() {
-        return ubicacion;
-    }
+    public void setCantidad(String cantidad) {this.cantidad = cantidad;}
 
-    public void setUbicacion(String ubicacion) {
-        this.ubicacion = ubicacion;
-    }
+    public String getUbicacion() {return ubicacion;}
+
+    public void setUbicacion(String ubicacion) {this.ubicacion = ubicacion;}
 
 
-
+    public TipoActividad getTa() {return ta;}
+    public void setTa(TipoActividad ta) {this.ta = ta;}
+    public CentroReciclaje getCr() {return cr;}
+    public void setCr(CentroReciclaje cr) {this.cr = cr;}
+    public User getU() {return u;}
+    public void setU(User u) {this.u = u;}
 
 
 }
