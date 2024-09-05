df9aa344aad750b8633db40723fd9bab0e03b23d
Aguila13 <jpierre.11@hotmail.com>
2024-09-04 21:06:58 -0500

    feat: update http request rewards

--- src/main/java/pe/edu/pe/grupo2/dtos/RecompensasDTO.java
+++ src/main/java/pe/edu/pe/grupo2/dtos/RecompensasDTO.java
@@ -1,5 +1,7 @@
 package pe.edu.pe.grupo2.dtos;
 
+import pe.edu.pe.grupo2.entities.Actividad;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 
@@ -8,6 +10,8 @@ public class RecompensasDTO {
     private String nombreRecompensa;
     private String codigoQR;
     private LocalDate fechaVencimiento;
+
+    private Actividad ac;
 
     public int getIdRecompensas() {
         return idRecompensas;
@@ -39,5 +43,13 @@ public class RecompensasDTO {
 
     public void setFechaVencimiento(LocalDate fechaVencimiento) {
         this.fechaVencimiento = fechaVencimiento;
+    }
+
+    public Actividad getAc() {
+        return ac;
+    }
+
+    public void setAc(Actividad ac) {
+        this.ac = ac;
     }
 }
