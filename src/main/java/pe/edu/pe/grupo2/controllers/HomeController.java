0816412ea7ba1f7b57d4de338a33f7a9a6887cc7
Heransue Geldres <149516843+hersite@users.noreply.github.com>
2024-09-17 00:31:05 -0500

    Update HomeController.java

--- src/main/java/pe/edu/pe/grupo2/controllers/HomeController.java
+++ src/main/java/pe/edu/pe/grupo2/controllers/HomeController.java
@@ -1,13 +1,13 @@
 package pe.edu.pe.grupo2.controllers;
+
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
-@RequestMapping("/")
 public class HomeController {
-    @GetMapping
-    public String home() {
-        return "redirect:/swagger-ui/index.html";
+    @GetMapping("/")
+    public RedirectView redirectToSwagger() {
+        return new RedirectView("/swagger-ui/index.html");
     }
 }
