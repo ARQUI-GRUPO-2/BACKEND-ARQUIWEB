6cc3cc84ff745a06e4483b77e64c3e22f6ed4063
Heransue Geldres <149516843+hersite@users.noreply.github.com>
2024-11-13 20:29:04 -0500

    feat: implement passwordbycript

--- src/main/java/pe/edu/pe/grupo2/controllers/UserController.java
+++ src/main/java/pe/edu/pe/grupo2/controllers/UserController.java
@@ -1,11 +1,15 @@
 package pe.edu.pe.grupo2.controllers;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import pe.edu.pe.grupo2.dtos.CantidadNotiUsuarioDTO;
 import pe.edu.pe.grupo2.dtos.UserCentroReciclajeDTO;
 import pe.edu.pe.grupo2.dtos.UserDTO;
 import pe.edu.pe.grupo2.entities.User;
+import pe.edu.pe.grupo2.repositories.UserRepository;
 import pe.edu.pe.grupo2.serviceinterfaces.UserService;
 
 import java.time.LocalDate;
@@ -19,6 +23,8 @@ import java.util.stream.Collectors;
 public class UserController {
     @Autowired
     private UserService uS;
+    @Autowired
+    private UserRepository uR;
 
     @PostMapping
     //  @PreAuthorize("hasAnyAuthority('USUARIO','ADMINISTRADOR')")
@@ -28,7 +34,7 @@ public class UserController {
         uS.insert(ur);
     }
 
-    @GetMapping
+   @GetMapping
     //   @PreAuthorize("hasAuthority('ADMINISTRADOR')")
     public List<UserDTO> listar() {
         return uS.list().stream().map(x -> {
@@ -37,13 +43,32 @@ public class UserController {
         }).collect(Collectors.toList());
     }
 
+
+   /* @GetMapping
+        public List<UserDTO> listar(@AuthenticationPrincipal User currentUser) {
+            ModelMapper m = new ModelMapper();
+
+            // Verificar el rol del usuario
+            if ("ADMI".equals(currentUser.getRol().getNombreRol())) {
+                // Si es admin, listar todos los usuarios
+                return uS.list().stream().map(x -> m.map(x, UserDTO.class)).collect(Collectors.toList());
+            } else if ("CLIENTE".equals(currentUser.getRol().getNombreRol())) {
+                // Si es cliente, listar solo su propio usuario
+                return List.of(m.map(currentUser, UserDTO.class));
+            }
+
+            // Si el rol no es válido, retornar lista vacía
+            return List.of();
+        }*/
+
     @GetMapping("/{id}")
     //   @PreAuthorize("hasAuthority('ADMINISTRADOR')")
     public UserDTO listarId(@PathVariable("id") Integer id) {
-        ModelMapper m=new ModelMapper();
-        UserDTO dto=m.map(uS.listId(id),UserDTO.class);
+        ModelMapper m = new ModelMapper();
+        UserDTO dto = m.map(uS.listId(id), UserDTO.class);
         return dto;
     }
+
     @PutMapping
     //  @PreAuthorize("hasAnyAuthority('USUARIO','ADMINISTRADOR')")
     public void modificar(@RequestBody UserDTO dto) {
@@ -51,9 +76,10 @@ public class UserController {
         User ur = m.map(dto, User.class);
         uS.update(ur);
     }
+
     @DeleteMapping("/{id}")
     //  @PreAuthorize("hasAnyAuthority('USUARIO','ADMINISTRADOR')")
-    public void eliminar(@PathVariable("id")Integer id) {
+    public void eliminar(@PathVariable("id") Integer id) {
         uS.delete(id);
     }
 
@@ -66,18 +92,7 @@ public class UserController {
             return m.map(x, UserDTO.class);
         }).collect(Collectors.toList());
     }
-
-    @GetMapping("/conteo_notificaciones_rangoHoras")
-    //   @PreAuthorize("hasAuthority('ADMINISTRADOR')")
-    public List<CantidadNotiUsuarioDTO> Cantidadnotificaciones_deusuario_rangohoras(@RequestParam LocalDate diaInicio, @RequestParam LocalDate diaFin) {
-        List<String[]> filaLista = uS.Cantidadnotificaciones_deusuario_rangohoras(diaInicio, diaFin);
-        List<CantidadNotiUsuarioDTO> dtoLista = new ArrayList<>();
-        for(String[] columna: filaLista){
-            CantidadNotiUsuarioDTO dto = new CantidadNotiUsuarioDTO();
-            dto.setNombres(columna[0]);
-            dto.setCantidad_notif(Integer.parseInt(columna[1]));
-            dtoLista.add(dto);
-        }
-        return dtoLista;
-    }
 }
+
+
+
